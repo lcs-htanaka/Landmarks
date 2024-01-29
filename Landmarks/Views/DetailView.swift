a94d10180baa3985c6809c2180a0c703a9a66a9a
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-29 12:00:11 -0500

    Applied abstraction to make detail view show properties of an instance of the Landmark structure.

--- Landmarks/Views/DetailView.swift
+++ Landmarks/Views/DetailView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    //MARK: STORED PROPERTIES
+    let item: Landmark
+    
+    //MARK: COMPUTED PROPERTIES
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            Image(item.image)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                    Image(systemName: item.isRecommended == true ? "hand.thumbsup.fill" : "hand.thumbsdown")
+
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 20)
+                    .padding(5)
+                
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text(item.description)
+            .padding(.horizontal)
+        }
+        .navigationTitle(item.name)
     }
 }
 
 #Preview {
-    DetailView()
+    NavigationStack {
+        DetailView(item: rom)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(item: sudburyNickel)
+    }
 }
