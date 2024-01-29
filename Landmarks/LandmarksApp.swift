14e7b85754634d86eb62fe864100e1b7bd0e811c
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-29 11:25:20 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 田中宏貴 on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
